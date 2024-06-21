605bc5868fc745ab28d1915f80ab2bd962b0bb08
YeonAnLee <tjsans9069@naver.com>
2024-06-21 14:53:35 +0900

    [수정] js 기능 이것저것

---
+++ assets/js/del_btn.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var con = document.querySelectorAll(".style_input")
+
+    // 인풋 태그에 입력이 발생할 때
+    con.forEach(function(con) {
+
+        var input = con.querySelector('.con_input');
+        var clearButton = con.querySelector('.del_btn');
+
+        input.addEventListener('input', function() {
+        if (input.value.length > 0) {
+            clearButton.style.display = 'block';
+        } else {
+            clearButton.style.display = 'none';
+        }
+        });
+
+        // span 태그를 클릭하면 인풋 태그의 값을 초기화
+        clearButton.addEventListener('click', function() {
+        input.value = '';
+        clearButton.style.display = 'none';
+        });
+
+    });
+
+});
